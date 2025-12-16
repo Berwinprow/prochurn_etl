a52941c1fcd44d764d15bd72b29d5b0a91d833c5
Swetha Mohan <swetha.mohan@prowesstics.com>
2025-12-16 17:37:44 +0530

    adding the changes to test1

--- dags/pipeline_lib.py
+++ dags/pipeline_lib.py
@@ -170,8 +170,7 @@ with DAG(
 ) as dag:
 
     # ---------------- START ----------------
-    with TaskGroup(group_id="start") as stage0_0_creation:
-        start = DummyOperator(task_id="start_pipeline")
+    start = DummyOperator(task_id="start_pipeline")
 
     # ---------------- Stage 0 ----------------
     with TaskGroup(group_id="schema_creation") as stage0_creation:
